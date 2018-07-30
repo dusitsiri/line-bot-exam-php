a32ec9b67b163d6fe17839df6ce7735ba4ae0101
dusitsiri <dusit.si@ku.th>
2018-07-30 16:31:52 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -6,7 +6,7 @@ require "vendor/autoload.php";
 
 $access_token = 'A0sMe4LtlYhOSY8f3hDTsuR8kRJ+vypeDOcIDWIab9Wh2zlB8WdHjH0ON7yLrEWQ60pN2QTN/dh35awNP9p1mCiWezqUQ4lAA8maOCtg1l2Jv9UjmX64649WHzVEUEqIPY0sL9A5EkxDy7RdmJ5w6gdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = 'd2b6f859c3b0833ab4d0d123acec33af';
+$channelSecret = '34a87537dc3059f5a9eb4611e3cd5713';
 
 $pushID = 'Ua2e9fc14ce9890b71d309631f0b8fe18';
 
