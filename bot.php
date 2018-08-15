657ca411f2121df91164fa09173e58b8c531c8b7
dusitsiri <dusit.si@ku.th>
2018-08-15 17:38:38 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -52,6 +52,12 @@
         $arrayPostData['messages'][0]['text'] = "ครับเจ้านาย";
         replyMsg($arrayHeader,$arrayPostData);
     }
+    else if($arrayJson['events'][0]['message']['text'] == "ด่าแจม" && $arrayJson['events'][0]['source']['userId']=="Ue14f54b7c3fe3f2219a3f42c016ab3a7"){
+        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "text";
+        $arrayPostData['messages'][0]['text'] = "แจมกาก กากแจม";
+        replyMsg($arrayHeader,$arrayPostData);
+    }
     else if($arrayJson['events'][0]['message']['text'] == "วันนี้วันอะไร" || $arrayJson['events'][0]['message']['text'] == "วันอะไร" || $arrayJson['events'][0]['message']['text'] == "วัน"){
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "text";
