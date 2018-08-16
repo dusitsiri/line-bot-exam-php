fa11fa415c468da2063158a7b2e93d7391e5c5c4
dusitsiri <dusit.si@ku.th>
2018-08-16 08:27:47 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -17,7 +17,7 @@
         $array = explode(" ",$str);
         $arrlength = count($array);
         for($x = 0; $x<$arrlength; $x++){
-            $arrayPostData['messages'][0]['text'] = $array[$x];
+            $arrayPostData['messages'][0]['text'] = $array[$x] .$arrayPostData['messages'][0]['text'];
         }
          replyMsg($arrayHeader,$arrayPostData);
       } 
