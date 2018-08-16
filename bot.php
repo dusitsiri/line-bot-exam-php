f5d97af0ab6d6304fdc23ec2a9a8e54410d69d17
dusitsiri <dusit.si@ku.th>
2018-08-16 22:01:31 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -86,7 +86,7 @@
     else if(strstr($message,"!potato") || strstr($message,"!Potato") || strstr($message,"!โปเตโต้")){
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "text";
-        $a=array("https://www.youtube.com/watch?v=UHKS37Inpdc&list=PLxLHpLGKZig0G9NvkGYZu4JhcHDyke-Og","https://www.youtube.com/watch?v=re1hIVppuc0","https://www.youtube.com/watch?v=1WMwDuwwAcM","https://www.youtube.com/watch?v=zYNsQ6ibdZs&start_radio=1&list=RDEMmLFELjkoyxs1joagnYi7Lg","https://www.youtube.com/watch?v=zYNsQ6ibdZs&start_radio=1&list=RDEMmLFELjkoyxs1joagnYi7Lg");
+        $a=array("https://www.youtube.com/watch?v=UHKS37Inpdc&list=PLxLHpLGKZig0G9NvkGYZu4JhcHDyke-Og?autoplay=1","https://www.youtube.com/watch?v=re1hIVppuc0?autoplay=1","https://www.youtube.com/watch?v=1WMwDuwwAcM?autoplay=1","https://www.youtube.com/watch?v=zYNsQ6ibdZs&start_radio=1&list=RDEMmLFELjkoyxs1joagnYi7Lg?autoplay=1","https://www.youtube.com/watch?v=zYNsQ6ibdZs&start_radio=1&list=RDEMmLFELjkoyxs1joagnYi7Lg?autoplay=1");
         $random_keys=array_rand($a);
         $arrayPostData['messages'][0]['text'] = $a[$random_keys];
         replyMsg($arrayHeader,$arrayPostData);
