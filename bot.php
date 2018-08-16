045f19df7fd6c450e8d36d0acb20666948814c47
dusitsiri <dusit.si@ku.th>
2018-08-16 21:51:14 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -93,9 +93,9 @@
     }
     else if(strstr($message,"!พี่ตูน") || strstr($message,"!Bodyslam") || strstr($message,"!bodyslam")){
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-        $arrayPostData['messages'][0]['type'] = "video";
-        $arrayPostData['messages'][0]['originalContentUrl'] = "https://www.youtube.com/embed/yf5JZDCXLG8&list?autoplay=1";
-        $arrayPostData['messages'][0]['previewImageUrl'] = "";
+        <iframe width="50" height="50"
+        src = "https://www.youtube.com/embed/yf5JZDCXLG8&list?autoplay=1">
+            </iframe>
         replyMsg($arrayHeader,$arrayPostData);
     }
     else if($arrayJson['events'][0]['message']['text'] == "วันนี้วันอะไร" || $arrayJson['events'][0]['message']['text'] == "วันอะไร" || $arrayJson['events'][0]['message']['text'] == "วัน"){
