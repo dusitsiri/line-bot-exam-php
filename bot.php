fd92c4f9232de0fd7f2186cf19c20bdad4f8e466
dusitsiri <dusit.si@ku.th>
2018-08-16 08:18:56 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,6 +19,7 @@
         for($x = 0; $x<$arrlength; $x++){
             $array[$x];
             $arrayPostData['messages'][0]['text'] =  "<br>";
+        }
         replyMsg($arrayHeader,$arrayPostData);
     }
     else if($arrayJson['events'][0]['message']['text'] == "Combus" || $arrayJson['events'][0]['message']['text'] ==  "combus" || $arrayJson['events'][0]['message']['text'] ==  "คอมบัส"){
